c87c674c3bf1cb8c3797d5e0297fbd1df72cd2ed
OnkarKalaskar <104019401+OnkarKalaskar@users.noreply.github.com>
2023-12-11 13:03:21 +0000

    second commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1231
+let email = 'onkar@123'
+var accountPassword = '1234'
+accountCity = 'pune'
+
+// accountId = 2
+email = 'fdfssddss'
+accountPassword = 2313
+accountCity = 'fdfdfd'
+
+console.log(accountId);
+console.table([accountId,email,accountPassword,accountCity])
+
+
